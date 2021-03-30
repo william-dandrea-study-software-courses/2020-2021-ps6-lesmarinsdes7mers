5a8e4c20a0e8d68f9a56198cb515172f42b74c34
Yann CLODONG <yann.clodong@gmail.com>
2021-03-30 09:19:21 +0200

    using revelant string for injecting css

--- front-end/src/models/handicap.enum.ts
+++ front-end/src/models/handicap.enum.ts
@@ -1,3 +1,18 @@
 export enum Handicap {
     NONE, PROTANOPIE, DEUTERANOPIE, TRITANOPIE, LOW_CONTRAST_SENSIBILITY
 }
+
+export function HandicapToString(handicap: Handicap) {
+    switch(handicap) {
+        case Handicap.NONE:
+            return "Aucun"
+        case Handicap.PROTANOPIE:
+            return "Protanopie"
+        case Handicap.DEUTERANOPIE:
+            return "Deuteranopie"
+        case Handicap.TRITANOPIE:
+            return "Tritanopie"
+        case Handicap.LOW_CONTRAST_SENSIBILITY:
+            return "Contraste"
+    }
+}
