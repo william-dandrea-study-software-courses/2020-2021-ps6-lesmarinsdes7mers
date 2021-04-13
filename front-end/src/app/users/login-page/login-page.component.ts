0f05083c3b0590ac0ba8b11b3da0c5dc6d630ddd
Gabriel Cogne <gabriel.cogne@gmail.com>
2021-04-13 12:38:23 +0200

    Add first login search bar implementation

--- front-end/src/app/users/login-page/login-page.component.ts
+++ front-end/src/app/users/login-page/login-page.component.ts
@@ -14,18 +14,18 @@ export default class LoginPageComponent implements OnInit{
 
     public userList: User[] = [];
 
-
-
     constructor(private router: Router, private userService: UserService, private userAndQuizService: UserAndQuizService) {
         this.userService.users$.subscribe((users) => {
-            this.userList = users;
+            users.forEach(user => this.userList.push(user))
         });
 
         this.userAndQuizService.oneUserQuizzes$.subscribe();
     }
 
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.filterUserByString('');
+    }
 
     logInGuestMode(): void {
         this.router.navigate(['guest', 'config', 'fontsize']);
@@ -44,5 +44,38 @@ export default class LoginPageComponent implements OnInit{
         this.router.navigate([url]);
 
 
+    }
+
+    filterUser(event: any) {
+        var value = event.target.value;
+
+        this.filterUserByString(value);
+    }
+
+    private filterUserByString (value: string) {
+        console.log("filter value: ", value);
+
+        this.userList.splice(0, this.userList.length);
+        if (value.length < 1) {
+            this.userService.users$.subscribe((users) => {
+                users.forEach(user => this.userList.push(user))
+            });
+
+            console.log("Bonjour");
+        }
+        else {
+            this.userService.users$.subscribe((users) => {
+                users.forEach(user => {
+                    var fullName = user.name + " " + user.surname;
+                    var revertFullName = user.surname + " " + user.name;
+                    if (fullName.toLowerCase().includes(value.toLowerCase()) ||
+                        revertFullName.toLowerCase().includes(value.toLowerCase())) {
+                        this.userList.push(user);
+                    }
+                })
+            });
+        }
+
+        console.log("users:", this.userList);
     }
 }
